36827cd0ba3f55e07d4453c67356dd4f39b56c7c
Sulym Dmytro <ds.intelserv@gmail.com>
2024-01-25 10:48:22 +0200

    Fixed bugs
    Fixed bugs

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,13 @@
-let getShippingMessage = (country, price, deliveryFee) => {
+function getShippingMessage(country, price, deliveryFee) {
   if (!isNaN(Number(price)) && !isNaN(Number(deliveryFee))) {
     const totalPrice = Number(price) + Number(deliveryFee);
     return `Shipping to ${country} will cost ${totalPrice} credits`;
   }
-  return "Error";
-};
+  return 'Error';
+}
 
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
-console.log(getShippingMessage("Australia", "100", "100"));
-console.log(getShippingMessage("Australia", "qwerty120", 10));
+console.log(getShippingMessage('Australia', 120, 50));
+console.log(getShippingMessage('Germany', 80, 20));
+console.log(getShippingMessage('Sweden', 100, 20));
+console.log(getShippingMessage('Australia', '100', '100'));
+console.log(getShippingMessage('Australia', 'qwerty120', 10));
--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
-let getElementWidth = (content, padding, border) => {
+function getElementWidth(content, padding, border) {
   const offsetWidth =
     parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-  return `${offsetWidth}`;
-};
+  return offsetWidth;
+}
 
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
